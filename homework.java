aa7315c3f1f3f9af21d4e6b2abc588648e4c406c
AlfonsoHassanik <92888389+AlfonsoHassanik@users.noreply.github.com>
2024-05-17 23:24:06 +0300

    Update homework.java

--- homework.java
+++ homework.java
@@ -5,3 +5,55 @@ public class Main {
         System.out.println("Banana");
         System.out.println("Apple");
     }
+//Задача №2
+    public class Main2 {
+        public static void main2(String[] args) {
+            checkSumSign();
+        }
+
+        public static void checkSumSign() {
+            int a = 5;
+            int b = 3;
+            int sum = a + b;
+            if (sum >= 0) {
+                System.out.println("Сумма положительная");
+            } else {
+                System.out.println("Сумма отрицательная");
+            }
+        }
+    }
+ //Задача №3
+public class Main {
+    public static void main(String[] args) {
+        printColor();
+    }
+
+    public static void printColor() {
+        int value = 111; 
+
+        if (value < 0) {
+            System.out.println("Красный");
+        } else if (value >= 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+}
+ //Задача №4
+public class Main {
+    public static void main(String[] args) {
+        compareNumbers();
+    }
+
+    public static void compareNumbers() {
+        int a = 10; // Можно изменить значение переменной a здесь
+        int b = 5; // Можно изменить значение переменной b здесь
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
