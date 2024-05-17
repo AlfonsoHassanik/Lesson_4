4da28e363f0aa77bfd9818c02f276ef1be38fcdc
AlfonsoHassanik <92888389+AlfonsoHassanik@users.noreply.github.com>
2024-05-17 22:49:34 +0300

    Create homework.java

---
+++ homework.java
@@ -0,0 +1,7 @@
+//Задача №1
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
